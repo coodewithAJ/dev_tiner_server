7b770308e3311fe9432dfcd75d81318c470c2d63
coodewithaj <coodewithaj@gmail.com>
2024-09-25 11:20:57 +0530

    exploring http calls

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 const express = require("express")
 const app = express();
 
+
 app.use("/home",(req,res)=>{
-    res.send("This is my home page")
+    res.send("This is the first call from home page")
 })
+
+app.use("/home/me",(req,res)=>{
+    res.send("Helo ashok bhai")
+})
+app.get("/home",(req,res)=>{
+    res.send("This is get call from home page")
+})
+app.post("/home",(req,res)=>{
+    res.send("This is post call from home page")
+})
+
 
 app.use("/test",(req,res)=>{
     res.send("This is my test page")
